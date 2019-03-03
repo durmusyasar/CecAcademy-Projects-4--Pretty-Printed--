fb44c6f7682964782db69ee071de8b36cdf703de
durmusyasar <durmusyasar13@gmail.com>
2019-03-03 23:10:04 +0300

    dokumantasyon2 yapildi

---
+++ modelvideo/example/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
